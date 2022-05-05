7cef36ed2d14f529a690a78c9fdb2cddf4f1cf86
Ghostling225 <wojcikadam742@gmail.com>
2022-05-05 19:53:32 +0200

    Rebuild projects screen with reusable component, fill out project props

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,32 +1,29 @@
-import { Card, CardActionArea, CardActions, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardActionArea, CardActions, CardMedia, CardContent, Typography, Button, Tooltip } from '@mui/material';
 import * as React from 'react';
 
 export default function ProjectCard(props) {
 	return (
 		<React.Fragment>
-			<Card sx={{ maxWidth: 400 }}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						height="225"
-						image={ props.img }
-						alt="Image"
-					/>
-					<CardContent>
-						<Typography variant="h5">
-							{ props.title }
-						</Typography>
-						<Typography>
-							{ props.description }
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary" href={ props.githubLink } target="_blank">
-						View on GitHub
-					</Button>
-				</CardActions>
-			</Card>
+			<Tooltip title="View on GitHub" placement="bottom" arrow>
+				<Card sx={{ maxWidth: 400, minWidth: 200 }}>
+					<CardActionArea href={ props.githubLink } target="_blank">
+						<CardMedia
+							component="img"
+							height="225"
+							image={ props.img }
+							alt="Image"
+						/>
+						<CardContent>
+							<Typography variant="h5">
+								{ props.title }
+							</Typography>
+							<Typography>
+								{ props.description }
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+				</Card>				
+			</Tooltip>
 		</React.Fragment>
 	)
 }
